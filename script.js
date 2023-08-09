010adc6973d55a8d4b5891fed4d0c56b05a4eb39
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-09 10:13:19 +0200

    Initializing html elements that will be used when displaying weather updates on web page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//getting most html element inorder to use them when displaying and updating fetched info
+let searchInput = document.querySelector('#search-input')
+let main = document.querySelector('main')
+let region = document.querySelector('.region')
+let date = document.querySelector('.date')
+let temp = document.querySelector('.temp')
+let image = document.querySelector('#image')
+let condition = document.querySelector('.text-condition')
+let sunRise = document.querySelector('#sunrise')
+let sunSet = document.querySelector('#sunset')
+let windSpeed = document.querySelector('#wind-speed')
+let windDirection = document.querySelector('#wind-direction')
+let visibility = document.querySelector('#visibility')
+let humidity = document.querySelector('#humidity')
+let uv = document.querySelector('#uv')
+let cover = document.querySelector('#cloud-cover')
+let hourlySection = document.querySelector('.forecast-details-hours')
+let hourlyTemp = document.querySelector('.forecast-details-temp')
+let searchBtn = document.querySelector('#search-btn')
+
+
 //declaring async fectData() function
 async function fetchData(){
 
