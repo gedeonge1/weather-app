83c107aa0d17bc129f583975f3b456ed9b7fd1bd
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-09 10:39:22 +0200

    displaying current weather data on webpage accodingly.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function fetchData(){
     //fetching data
     let promise = await fetch(url, {mode: 'cors'})
 
-     //handling error if user passes in invalid location
+    //handling error if user passes in invalid location
      if(!promise.ok){
         main.style.display = 'none' ;
         error.style.display = 'block'
@@ -38,8 +38,26 @@ async function fetchData(){
      }else if(promise.status == 200){
         error.style.display = 'none'
         main.style.display = 'block'
-     //converting resolved json data into object 
+        
+        //converting resolved json data into object 
         let result = await promise.json();
+
+        //displaying current weather data on webpage accodingly.
+        searchInput.value = ''
+        time = result.location.localtime
+        region.textContent = `${result.location.country}, ${result.location.name}`
+        date.textContent = `${ time}`
+        temp.textContent = `${result.current.temp_c}°C`
+        image.src = result.current.condition.icon
+        condition.textContent = result.current.condition.text
+        sunRise.textContent = result.forecast.forecastday[0].astro.sunrise
+        sunSet.textContent = result.forecast.forecastday[0].astro.sunset
+        windDirection.textContent = result.current.wind_dir
+        windSpeed.textContent = `${result.current.wind_kph} kph`
+        visibility.textContent = `${result.current.vis_km} km`
+        humidity.textContent = result.current.humidity
+        uv.textContent = result.current.uv
+        cover.textContent = `${result.current.cloud}%`
     
 }
 }
